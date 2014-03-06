c9a6c061ee43471b068f5652afca76c7db43d535
Emerson R Orci <emersonorci@hotmail.com>
2014-03-06 06:01:13 -0300

    First commit.

---
+++ src/com/livro/capitulo3/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package com.livro.capitulo3.util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			//Configuration cfg = new Configuration();
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+		} catch (Throwable e) {
+			System.out.println("Criação inicial do objeto SessionFactory falhou.\nErro: " + e);
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
