20144c1b3c16e5976813733369dba5b58d98ae0e
Emerson R Orci <emersonorci@hotmail.com>
2014-03-06 15:01:12 -0300

    Creating Filme and Categoria class.

---
+++ src/com/livro/capitulo3/categoria/Categoria.java
@@ -0,0 +1,94 @@
+package com.livro.capitulo3.categoria;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = -6658831328735473555L;
+	
+	@Id
+	@GeneratedValue
+	@Column(name="cod_categoria")
+	private Integer categoria;
+	
+	@Column(name="descricao", length=50, nullable=true)
+	private String descricao;
+
+	public Integer getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Integer categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Categoria)) {
+			return false;
+		}
+		Categoria other = (Categoria) obj;
+		if (categoria == null) {
+			if (other.categoria != null) {
+				return false;
+			}
+		} else if (!categoria.equals(other.categoria)) {
+			return false;
+		}
+		if (descricao == null) {
+			if (other.descricao != null) {
+				return false;
+			}
+		} else if (!descricao.equals(other.descricao)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+
+}
